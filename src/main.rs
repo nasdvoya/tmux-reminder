e127aa949b98372fdbba45b5fbd6ce717e9c01cf
sasha <nasdvoya@proton.me>
2025-03-06 16:11:17 +0000

    Sync

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use std::{
-    env, fs,
+    env::{self, args},
+    fs,
     process::Command,
     thread,
     time::{Duration, SystemTime},
@@ -9,17 +10,15 @@ fn get_file_age(file: &String) -> Result<(), Box<dyn std::error::Error>> {
     let meta = fs::metadata(file)?;
     let changed = meta.modified()?;
     let sys_time = SystemTime::now().duration_since(changed)?;
-
-    println!("{:?}", sys_time);
-
     Ok(())
 }
 
 fn main() {
+    let args: Vec<String> = env::args().collect();
     let home = env::var("HOME").expect("Coulld not get home directory");
     let file_path = format!("{home}/Github/nasdvoya/tmux-reminder/flake.nix");
 
-    get_file_age(&file_path);
+    // get_file_age(&file_path);
 
     let content = fs::read_to_string(&file_path).unwrap_or_else(|e| {
         eprintln!("Failed to read file: {}", e);
@@ -30,27 +29,15 @@ fn main() {
 
     let line_content: Vec<String> = content
         .lines()
-        .skip(10) // Skip the header
+        .skip(2) // Skip the header
         .filter(|line| !line.trim().is_empty())
         .map(String::from)
         .collect();
 
-    let output = Command::new("tmux")
-        .arg("show-option")
-        .arg("-v")
-        .arg("status")
-        .output()
-        .expect("Failed to execute tmux");
-
-    let _result = String::from_utf8(output.stdout)
-        .expect("Failed to parse output")
-        .trim()
-        .to_string();
-
     thread::spawn(move || {
         loop {
-            fs::metadata(&file_path);
             for reminder in &line_content {
+                println!("{}", reminder);
                 _ = Command::new("tmux")
                     .arg("set")
                     .arg("-g")
@@ -64,22 +51,6 @@ fn main() {
             }
         }
     });
-
-    // println!("{:?}",result);
-
-    // if result == "2" {
-    //     _ = Command::new("tmux")
-    //         .arg("set-option")
-    //         .arg("status")
-    //         .arg("on")
-    //         .output();
-    // } else {
-    //     _ = Command::new("tmux")
-    //         .arg("set-option")
-    //         .arg("status")
-    //         .arg("2")
-    //         .output();
-    // }
 }
 
 // Center
--- tmux-reminder.tmux
+++ tmux-reminder.tmux
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
 
 CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-tmux bind-key t run-shell "$CURRENT_DIR/target/release/tmux_reminder"
+
+tmux set-option -gq @tmux_reminder_interval "5"
+tmux set-option -gq @tmux_reminder_file "$CURRENT_DIR/flake.nix"
+
+tmux bind-key q run-shell "$CURRENT_DIR/target/release/tmux_reminder"
