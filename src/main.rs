3a7e9cafba7b0676db9f6d210cfe232a36e6108f
sasha <nasdvoya@proton.me>
2025-03-07 15:58:00 +0000

    Sync

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,11 @@
 use std::{
     env::{self},
+    fmt::format,
     fs,
     process::Command,
     thread,
     time::{self, Duration, SystemTime},
 };
-
-fn get_file_age(file: &String) -> Result<Duration, Box<dyn std::error::Error>> {
-    let meta = fs::metadata(file)?.modified()?;
-    let time = SystemTime::now().duration_since(meta)?;
-    Ok(time)
-}
 
 struct Configuration {
     file_path: String,
@@ -45,59 +40,66 @@ fn setup() -> Result<Configuration, String> {
     })
 }
 
-fn main() {
-    let args: Vec<String> = env::args().collect();
-    let home = env::var("HOME").expect("Coulld not get home directory");
-    let file_path = format!("{home}/Github/nasdvoya/tmux-reminder/flake.nix");
+fn get_file_age(file: &String) -> Result<Duration, Box<dyn std::error::Error>> {
+    let meta = fs::metadata(file)?.modified()?;
+    let time = SystemTime::now().duration_since(meta)?;
+    Ok(time)
+}
 
-    let content = fs::read_to_string(&file_path).unwrap_or_else(|e| {
-        eprintln!("Failed to read file: {}", e);
-        String::new()
-    });
-
+fn get_file_content(file_path: &String) -> Result<Vec<String>, Box<dyn std::error::Error>> {
+    let content = fs::read_to_string(&file_path)?;
     dbg!(&content);
 
     let line_content: Vec<String> = content
         .lines()
-        .skip(2) // Skip the header
+        .skip(2)
         .filter(|line| !line.trim().is_empty())
         .map(String::from)
         .collect();
 
-    thread::spawn(move || {
-        loop {
-            // get_file_age(&file_path);
-            // if get_file_age(&file_path) >= Time {
-            //
-            // }
-            if let Ok(age) = get_file_age(&file_path) {
-                if age >= SystemTime::now().duration_since()   {
-                    todo!();
-                }
-            } else {
-            }
-
-            for reminder in &line_content {
-                println!("{}", reminder);
-                _ = Command::new("tmux")
-                    .arg("set")
-                    .arg("-g")
-                    .arg("status-right")
-                    .arg(format!(
-                        "#[align=absolute-centre] {} #[align=right]",
-                        reminder
-                    ))
-                    .output();
-                thread::sleep(Duration::from_secs(2));
-            }
-        }
-    });
+    return Ok(line_content);
 }
 
-// How Your Rust Code Should Handle These Options
-//
-// Your Rust program should retrieve these values using:
-//
+fn main() {
+    let plugin_configuration = match setup() {
+        Ok(config) => config,
+        Err(e) => {
+            eprintln!("Error during plugin setup, will use defaults, error: {}", e);
+            let home = env::var("HOME").expect("Coulld not get home directory");
+            Configuration {
+                file_path: format!("{home}/Github/nasdvoya/tmux-reminder/flake.nix"),
+                update_interval: 5,
+            }
+        }
+    };
+
+    if let Ok(notes_content) = get_file_content(&plugin_configuration.file_path) {
+        thread::spawn(move || {
+            loop {
+                if let Ok(file_age) = get_file_age(&plugin_configuration.file_path) {
+                    if file_age >= Duration::from_secs(5) {
+                        let notes_content = get_file_content(&plugin_configuration.file_path);
+                    }
+                } else {
+                }
+
+                for reminder in &notes_content {
+                    _ = Command::new("tmux")
+                        .arg("set")
+                        .arg("-g")
+                        .arg("status-right")
+                        .arg(format!(
+                            "#[align=absolute-centre] {} #[align=right]",
+                            reminder
+                        ))
+                        .output();
+                    thread::sleep(Duration::from_secs(2));
+                }
+            }
+        });
+    } else {
+    }
+}
 
 // Center
 // set -g status-right "#[align=absolute-centre] Hello, world! #[align=right]"
